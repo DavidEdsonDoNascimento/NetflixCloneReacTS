00d9d5eda035f81f1d60bd941c09414e12f1ab04
David Edson <david.contato.tec@gmail.com>
2021-09-18 15:59:04 -0300

    FEAT: master - add css in the app and movie sections

--- src/components/MovieSection.component.tsx
+++ src/components/MovieSection.component.tsx
@@ -2,12 +2,15 @@ import './MovieSection.css';
 import Movie from './../interfaces/Movie';
 
 export default ({ slugs, title, items }: Movie) =>
-(<ul>
-    <li>{title}</li>
-    <div className="movieSection--rowlist">
-        {//items?.results?.map((item, key) => {
-            // return <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} />
-            //})
-        }
+(<div className="movieSection">
+    <h2>{title}</h2>
+    <div className="movieSection--listarea">
+        <div className="movieSection--list">
+            {items?.results?.map((item: any, key: string) => {
+                return <div key={key} className="movieSection--item">
+                    <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
+                </div>
+            })}
+        </div>
     </div>
-</ul>);
+</div>);
