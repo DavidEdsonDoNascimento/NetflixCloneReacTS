0f1905cc556a84f14c5cd9a98bd897fcd89bd6a3
DavidEdsonDoNascimento <david.contato.tec@gmail.com>
2021-10-06 18:38:30 -0300

    FEAT: master - add media query to render mobile

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,8 @@
+import './Loading.css';
+
+const Loading = () => 
+<div className="loading">
+    <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="loading ..." />
+</div>
+
+export default Loading;
